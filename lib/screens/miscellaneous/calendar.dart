9559cedad17e6a087d9e29ff56c4ac394b46f9f5
SohamDesai1 <sohamdesai123@gmail.com>
2024-03-23 16:31:38 +0530

    feat(firestore): added dynamic fetching

--- lib/screens/miscellaneous/calendar.dart
+++ lib/screens/miscellaneous/calendar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:tuple/tuple.dart';
 
 class Calendar extends StatefulWidget {
   const Calendar({super.key});
@@ -10,8 +12,24 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
-  List year = ["ODD Year", "EVEN Year"];
-  // List link = ["ODD Year", "EVEN Year"];
+  late String oddLink;
+  late String evenLink;
+  List<String> links = [];
+
+  Future<Tuple2<List<String>, List<String>>> getLink() async {
+    List<String> year = ["ODD Year", "EVEN Year"];
+    var db = FirebaseFirestore.instance;
+    final docref = db.collection('utils').doc('pdfs');
+    final doc = await docref.get();
+    final data = doc.data();
+    oddLink = data!['odd_year_tt'];
+    links.add(oddLink);
+    evenLink = data['even_year_tt'];
+    links.add(evenLink);
+
+    return Tuple2<List<String>, List<String>>(year, links);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -28,39 +46,56 @@ class _CalendarState extends State<Calendar> {
             ),
           ),
         ),
-        body: Padding(
-          padding: EdgeInsets.only(
-            top: 1.5.h,
-          ),
-          child: SizedBox(
-            height: 55.h,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    InkWell(
-                      onTap: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const PDFScreen(),
-                          )),
-                      child: Container(
-                        height: 6.h,
-                        decoration: BoxDecoration(
-                            border: Border.all(),
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Center(child: Text(year[index])),
-                      ),
-                    ),
-                    SizedBox(
-                      height: 3.h,
-                    )
-                  ],
-                );
-              },
-              itemCount: year.length,
-            ),
-          ),
+        body: FutureBuilder<Tuple2<List<String>, List<String>>>(
+          future: getLink(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              var years = snapshot.data!.item1;
+              var links = snapshot.data!.item2;
+              return Padding(
+                padding: EdgeInsets.only(
+                  top: 1.5.h,
+                ),
+                child: SizedBox(
+                  height: 55.h,
+                  child: ListView.builder(
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          InkWell(
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => PDFScreen(
+                                    text: years[index],
+                                    link: links[index],
+                                  ),
+                                )),
+                            child: Container(
+                              height: 6.h,
+                              width: 80.w,
+                              decoration: BoxDecoration(
+                                  border: Border.all(),
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Center(child: Text(years[index])),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 3.h,
+                          )
+                        ],
+                      );
+                    },
+                    itemCount: years.length,
+                  ),
+                ),
+              );
+            } else if (snapshot.hasError) {
+              return const Center(child: Text("Error fetching categories"));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
     );
@@ -68,14 +103,18 @@ class _CalendarState extends State<Calendar> {
 }
 
 class PDFScreen extends StatelessWidget {
-  const PDFScreen({super.key});
+  final String text;
+  final String link;
+  const PDFScreen({super.key, required this.text, required this.link});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: SfPdfViewer.network(
-          'https://firebasestorage.googleapis.com/v0/b/college-manager-b1302.appspot.com/o/Academic%20Calendar-Odd%20Sem-23-24.pdf?alt=media&token=3babbf44-9768-4740-96be-8ee2657aabf0'),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
+        title: Text(text),
+      ),
+      body: SfPdfViewer.network(link),
     );
   }
 }
