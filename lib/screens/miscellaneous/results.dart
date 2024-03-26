b1ecfd5328770c36dfcf409ce8e21bedccca4045
SohamDesai1 <sohamdesai123@gmail.com>
2024-03-26 16:52:10 +0530

    feat(results): dynamic fetching from firestore

--- lib/screens/miscellaneous/results.dart
+++ lib/screens/miscellaneous/results.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
@@ -9,23 +10,97 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
+  List<String> results = [];
+
+  Future<List> getreults() async {
+    var db = FirebaseFirestore.instance;
+    final docref = db.collection('utils').doc('results');
+    final doc = await docref.get();
+    final data = doc.data();
+    results.add(data!['BE']);
+    results.add(data['TE']);
+    results.add(data['SE']);
+    return results;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xC90000FF),
+        iconTheme: const IconThemeData(color: Colors.black),
         title: Center(
           child: Padding(
-            padding: EdgeInsets.only(right: 10.w),
+            padding: EdgeInsets.only(right: 12.w),
             child: const Text(
               "Results",
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Colors.black),
             ),
           ),
         ),
       ),
-      body: SizedBox(
-        child: Image.asset("assets/images/results.jpeg"),
+      body: SingleChildScrollView(
+        child: Center(
+          child: FutureBuilder(
+              future: getreults(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  var links = snapshot.data!;
+                  return Column(
+                    children: [
+                      SizedBox(
+                        child: Image.network(
+                          links[0],
+                          fit: BoxFit.contain,
+                          height: 53.h,
+                          errorBuilder: (ctx, s, err) {
+                            return const Text("No results found");
+                          },
+                        ),
+                      ),
+                      const Center(
+                        child: Text("FINAL YEAR"),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      SizedBox(
+                        child: Image.network(
+                          links[0],
+                          fit: BoxFit.contain,
+                          height: 53.h,
+                          errorBuilder: (ctx, s, err) {
+                            return const Text("No results found");
+                          },
+                        ),
+                      ),
+                      const Center(
+                        child: Text("THIRD YEAR"),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      SizedBox(
+                        child: Image.network(
+                          links[0],
+                          fit: BoxFit.contain,
+                          height: 53.h,
+                          errorBuilder: (ctx, s, err) {
+                            return const Text("No results found");
+                          },
+                        ),
+                      ),
+                      const Center(
+                        child: Text("SECOND YEAR"),
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return const Center(child: Text("Error fetching categories"));
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              }),
+        ),
       ),
     );
   }
